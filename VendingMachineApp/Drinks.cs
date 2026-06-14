1cede1a964a390b41aaed8258a0f6402dd92225c
Marina <aminovamarina2006@gmail.com>
2026-06-15 05:29:52 +0800

    начало

--- VendingMachineApp/Drinks.cs
+++ VendingMachineApp/Drinks.cs
@@ -4,9 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+
 namespace VendingMachineApp
 {
-    internal class Drinks
+    public class Drink
     {
+        public static Random rnd = new Random();
+        public int Volume = 0;
+
+        public virtual string GetInfo()
+        {
+            return $"Объём: {Volume} мл";
+        }
     }
 }
